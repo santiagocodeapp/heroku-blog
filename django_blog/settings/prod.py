385d055bf06420c9d42c5f66af5732c442f1b2f1
Santiago Nessy <santiago.codeapp@gmail.com>
2022-04-11 09:41:26 -0400

    Heroku Settings

--- django_blog/settings/prod.py
+++ django_blog/settings/prod.py
@@ -1,3 +1,4 @@
+from cgitb import handler
 from django_blog.settings.dev import ALLOWED_HOSTS, SECRET_KEY
 import django_on_heroku
 from decouple import config
@@ -12,3 +13,35 @@ ALLOWED_HOSTS = [
     'django-basic-blog.herokuapp.com'
 ]
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datemt': "%d/%b/ %Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handler': ['console'],
+            'level': 'DEBUG'
+        },
+    }
+}
+
+# Heroku settings
+
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
